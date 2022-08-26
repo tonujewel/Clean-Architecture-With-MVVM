4d8b32568786782bad9ec3617b9677f41b202e80
jewelrana <tonujewel@gmail.com>
2022-08-27 02:57:36 +0600

    App theme manager

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,24 +1,97 @@
 import 'package:clean_architecture_with_mvvm/presentation/color_manager.dart';
+import 'package:clean_architecture_with_mvvm/presentation/font_manager.dart';
+import 'package:clean_architecture_with_mvvm/presentation/style_manager.dart';
 import 'package:flutter/material.dart';
+
+import 'values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main color of the app
-      primaryColor: ColorManager.primar,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1,
-      colorScheme:
-          ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey)
+    // main color of the app
+    primaryColor: ColorManager.primar,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.primaryOpacity70, // ripple color
+    colorScheme:
+        ColorScheme.fromSwatch().copyWith(secondary: ColorManager.grey),
 
-      // card view theme
+    // card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
 
-      // appbar theme
+    // App bar theme
+    appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primar,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle: getRegularTextStyle(
+          color: ColorManager.white,
+          fontSize: FontSize.s16,
+        )),
 
-      // button theme
+    // Button theme
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
+      disabledColor: ColorManager.grey,
+      buttonColor: ColorManager.primar,
+      splashColor: ColorManager.primaryOpacity70,
+    ),
 
-      // text theme
+    // Elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularTextStyle(color: ColorManager.white),
+        primary: ColorManager.primar,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+      ),
+    ),
 
-      // input decoration theme (text form field)
-      );
+    // text theme
+    textTheme: TextTheme(
+      headline1: getSemiBoldTextStyle(
+          color: ColorManager.darkGrey, fontSize: FontSize.s16),
+      subtitle1: getMediumTextStyle(
+          color: ColorManager.lightGrey, fontSize: FontSize.s14),
+      caption: getRegularTextStyle(color: ColorManager.grey1),
+      bodyText1: getRegularTextStyle(color: ColorManager.grey),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(AppSize.s4),
+      hintStyle: getRegularTextStyle(color: ColorManager.grey1),
+      labelStyle: getRegularTextStyle(color: ColorManager.darkGrey),
+      // error
+      errorStyle: getRegularTextStyle(color: ColorManager.error),
+
+      // enable border
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s4)),
+      ),
+
+      // Focus border
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.primar, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s4)),
+      ),
+
+      // Focus border
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.error, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s4)),
+      ),
+
+      //Focus border
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: ColorManager.primar, width: AppSize.s1_5),
+        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s4)),
+      ),
+    ),
+  );
 }
--- lib/presentation/values_manager.dart
+++ lib/presentation/values_manager.dart
@@ -1,4 +1,3 @@
-
 class AppMargin {
   static const double m8 = 8.0;
   static const double m10 = 10.0;
@@ -20,6 +19,8 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s1_5 = 1.5;
+  static const double s4 = 4.0;
   static const double s8 = 8.0;
   static const double s10 = 10.0;
   static const double s12 = 12.0;
