5682f4a39d8a9a8150659cff8beea0f800f16918
jewelrana <tonujewel@gmail.com>
2022-09-17 12:53:41 +0600

    feat: base view model

---
+++ lib/presentation/base_view_model/base_view_model.dart
@@ -0,0 +1,12 @@
+abstract class BaseViewModel {
+// Share variables and functions that will be used through any view model.
+
+}
+
+abstract class BaseViewModelInputs {
+  void start(); // will be called while init of view model.
+  void dispose(); // will be called when viewmodel dies.
+  
+}
+
+abstract class BaseViewModelOutputs {}
