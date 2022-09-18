37f0432aae22f7b94e15345da551a36603aa688b
jewelrana <tonujewel@gmail.com>
2022-09-19 00:53:46 +0600

    Domain Layer - Creating Models

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -5,3 +5,39 @@ class SliderObject {
 
   SliderObject(this.title, this.subTitle, this.image);
 }
+
+class User {
+  int id;
+  String firstName;
+  String lastName;
+  String role;
+  String email;
+  String phone;
+  String balance;
+  String currencyCode;
+  double lat;
+  double lng;
+  String image;
+  String token;
+
+  User(
+    this.id,
+    this.firstName,
+    this.lastName,
+    this.role,
+    this.email,
+    this.phone,
+    this.balance,
+    this.currencyCode,
+    this.lat,
+    this.lng,
+    this.image,
+    this.token,
+  );
+}
+
+class Authentication {
+  User user;
+
+  Authentication(this.user);
+}
