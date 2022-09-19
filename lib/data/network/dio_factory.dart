bb09f5af8c42d21fbacc384218590fa46b6562d0
jewelrana <tonujewel@gmail.com>
2022-09-19 21:42:43 +0600

    Adding Dio Logger Interceptor

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,5 +1,7 @@
 import 'package:clean_architecture_with_mvvm/app/constant.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 const String APPLICATION_JSON = "application/jso";
 const String CONTENT_TYPE = "content-type";
@@ -25,6 +27,18 @@ class DioFactory {
       receiveTimeout: _timeOut,
       headers: header,
     );
+
+    if (kReleaseMode) {
+      print("Release mode no logs");
+    } else {
+      dio.interceptors.add(
+        PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseBody: true,
+        ),
+      );
+    }
 
     return dio;
   }
