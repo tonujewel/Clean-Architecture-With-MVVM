14442fcd5d6dd4ccf2a2b3f4cc89be1f7ac0c5de
Md Jewel Rana <jewel.singularity@gmail.com>
2022-10-26 16:54:18 +0600

    [Added] Retrieving Caching Data from Local Data Source

--- lib/data/data_sources/local_data_source.dart
+++ lib/data/data_sources/local_data_source.dart
@@ -1,6 +1,9 @@
+import 'package:clean_architecture_with_mvvm/data/network/error_handler.dart';
+
 import '../responses/response.dart';
 
 const CACHE_RESTAURANT_KEY = 'CACHE_RESTAURANT_KEY';
+const CACHE_RESTAURANT_INTERVAL = 60*1000;
 
 abstract class LocalDataSource {
   Future<RestaurantDataResponse> getRestaurantData();
@@ -13,8 +16,15 @@ class LocalDataSourceImplementer implements LocalDataSource {
   Map<String, CacheItem> cacheMap = Map();
   @override
   Future<RestaurantDataResponse> getRestaurantData() {
-    // TODO: implement getRestaurantData
-    throw UnimplementedError();
+    CacheItem? cacheItem = cacheMap[CACHE_RESTAURANT_KEY];
+
+    if (cacheItem != null && cacheItem.isValid(CACHE_RESTAURANT_INTERVAL)) {
+      return cacheItem.data;
+      // return response
+    } else {
+      // return error that cache is not valid
+      throw ErrorHandler.handle(DataSource.CACHE_ERROR);
+    }
   }
 
   @override
@@ -30,4 +40,14 @@ class CacheItem {
   int cacheTime = DateTime.now().millisecondsSinceEpoch;
 
   CacheItem(this.data);
+}
+
+extension CacheItemExtension on CacheItem {
+  bool isValid(int expirationTime) {
+    int currentTimeInMili = DateTime.now().millisecondsSinceEpoch;
+
+    bool isCacheValid = currentTimeInMili - expirationTime < cacheTime;
+
+    return isCacheValid;
+  }
 }
